4f1187b879b71d51a539c13fa44e1ff290cf4fca
John Rojcewicz <rojcewiczj@gmail.com>
2020-09-05 18:22:53 -0500

    got core functionality working in console

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,54 +1,71 @@
-import React, {useState, useCallback, useRef} from 'react';
+import React, {useState, useCallback, useREntity} from 'react';
 import produce from 'immer';
 import logo from './logo.svg';
 import './App.css';
-import {Ef} from './Ef'
+import {Entity} from "./Entity"
 
-const numRows = 20;
-const numCols = 20;
+
 
 
 function App() {
-  const [grid, setGrid] = useState(()=> {
-    const rows = [];
-    for (let i=0; i < numRows; i++){
-      rows.push(Array.from(Array(numCols), ()=> 0));
-    }
-    return rows
-  });
-  const [userInput, setInput] = useState("")
 
- const points = 0
- const connections = {}
+const grid = []
+const [userInput, setInput] = useState("")
+for(let i = 0; i < 20; i++){
+  const row = []
+  if(i == 0 || i == 19) {
+    for(let j = 0; j < 20; j++){
+      row.push("X")
+  }}
+  else{
+    for(let j = 0; j < 20; j++){
+      row.push(".")
+  }}
+row[0] = "X"
+row[row.length -1] = "X"
+grid.push(row)
+}
 
- for(let x = 0; x < grid.length; x ++){
-   for(let y = 0; y < grid[x].length; y++){
-      connections[`${x}, ${y}`] = []
-      if((x + 1) < 20){
-      connections[`${x}, ${y}`].push([x + 1,  y])
+function is_not_ex(x, y){
+  if (grid[x][y] != "X"){
+    return true
+  }
+  else{
+    return false
+}}
+
+function insert(x, y, elem){
+  grid[x][y] = elem.value
+  elem.location = [x, y]
+}
+const connections = {}
+  for (let x = 0; x < grid.length ; x ++){
+    for(let y  = 0; y < grid[x].length ; y ++){
+      if (is_not_ex(x, y)){
+        connections[`${x},${y}`] = []
+        if(is_not_ex(x +1, y)){
+          connections[`${x},${y}`].push([x + 1,  y])
+        }
+        if (is_not_ex(x -1, y)){
+          connections[`${x},${y}`].push([x - 1 , y])
+        }
+        if (is_not_ex(x , y + 1)){
+          connections[`${x},${y}`].push([x  , y + 1])
+        }
+        if (is_not_ex(x, y - 1)){
+          connections[`${x},${y}`].push([x , y - 1])
+        }
+  
       }
-       if((x - 1) > 0){
-      connections[`${x}, ${y}`].push([x - 1 , y])
-       }
-       if((y +1) < 20){
-      connections[`${x}, ${y}`].push([x , y + 1])
-       }
-       if((y -1) > 0){
-      connections[`${x}, ${y}`].push([x , y - 1])
     }
-   }
- }
- function insert(x,y,value){
-   grid[x][y] = value.value
-   value.location = [x,y]
- }
- const elem = new Ef("F", grid, connections,userInput)
- const enem = new Ef("W", grid, connections, userInput)
- const enem1 = new Ef("W", grid, connections, userInput)
- const enem2 = new Ef("W", grid, connections, userInput)
- const enem3 = new Ef("W", grid, connections, userInput)
- const money = new Ef("$", grid, connections, userInput)
- const ex = new Ef("X", grid, connections, userInput)
+  }
+const elem = new Entity("P", grid, connections, userInput)
+const enem = new Entity("W", grid, connections, userInput)
+const enem1 = new Entity("W", grid, connections, userInput)
+const enem2 = new Entity("W", grid, connections, userInput)
+const enem3 = new Entity("W", grid, connections, userInput)
+const money = new Entity("$", grid, connections, userInput)
+const ex = new Entity("X", grid, connections)
 insert(1,2, elem)
 insert(3,3, ex)
 insert(7,4, ex)
@@ -62,9 +79,6 @@ insert(17,2, ex)
 insert(14,6, ex)
 insert(17,9, ex)
 insert(4,5, enem)
-insert(2,3, enem1)
-insert(5,6, enem2)
-insert(15,16, enem3)
 insert(4,5, money)
 insert(2,6, money)
 insert(5,8, money)
@@ -76,57 +90,36 @@ insert(16,3, money)
 insert(6,13, money)
 insert(9,9, money)
 insert(17,12, money)
+
+elem.get_neighbors()
+
+function runFunctions(string){
+  setInput(string)
+  elem.move(elem, enem)
+  enem.move(enem, elem)
+  setInput(string)
+  console.log(elem.map)
+}
+
+
+return(
+  <div>
+  <button onClick ={()=>{runFunctions("w")}}>
+  W
+  </button>
+  <button onClick ={()=>{runFunctions("s")}}>
+  S
+  </button>
+  <button onClick ={()=>{runFunctions("d")}}>
+  D
+  </button>
+  <button onClick ={()=>{runFunctions("a")}}>
+  A
+  </button>
  
- elem.get_neighbors()
-
- console.log(grid)
- console.log(connections)
- const [running, setRunning] = useState(false);
- const runningRef = useRef(running);
- runningRef.current = running
-
- const runGame = useCallback(()=> {
-   if (!runningRef.current) {
-     return;
-   }
-   elem.move(elem, enem)
-   enem.move(enem, elem)
-   enem1.move(enem1, elem)
-   enem2.move(enem2, elem)
-   enem3.move(enem3, elem)
-   setTimeout(runGame, 1000);
- }, [])
-
-  return (
-    <>
-    <button onClick={()=>{
-      setRunning(!running);
-    }}
-    >
-      {running ? "stop": "start"}</button>
-    <div style={{
-      display: 'grid',
-      gridTemplateColumns: `repeat(${numCols}, 20px)`
-    }}>
-     {grid.map((rows, i) => rows.map((col, k) =>( 
-     <div 
-     keys={`${i}-${k}`} 
-     onClick = {()=> {
-       const newGrid = produce(grid, gridCopy => {
-         gridCopy[i][k] = grid[i][k] ? 0 : 1 ;
-       })
-     setGrid(newGrid)
-     }}
-     style={{
-       width:20, 
-       height: 20, 
-       backgroundColor: grid[i][k] ? "pink" : undefined,
-       border: "solid 1px black"}} />
-     ))
-     )}
-    </div>
-    </>
-  );
+</div>
+);
+  
 }
 
 export default App;
---
+++ my-app/src/Entity.js
@@ -0,0 +1,129 @@
+import React from "react";
+
+
+export class Entity{
+    constructor(value, mappy, connections, userInput){
+        this.value = value
+        this.connections = connections
+        this.map = mappy
+        this.location = []
+        this.neighbors = []
+        this.points = 0
+        this.userInput = userInput
+    
+    }
+    get_neighbors(){
+        this.neighbors = []
+        this.neighbors.push([this.map[this.location[0] + 1][this.location[1]], `${this.location[0] + 1},${this.location[1]}`])
+        this.neighbors.push([this.map[this.location[0] - 1][this.location[1]], `${this.location[0] - 1},${this.location[1]}`])
+        this.neighbors.push([this.map[this.location[0]][this.location[1] + 1], `${this.location[0]},${this.location[1] + 1}`])
+        this.neighbors.push([this.map[this.location[0]][this.location[1] - 1], `${this.location[0]},${this.location[1] - 1}`])
+    }
+    move (elem, enem){
+        
+        this.get_neighbors()
+        function list(iterable) {
+            return [...iterable];  
+          }
+        let directions = {
+            "w":    this.neighbors[1][0],
+            "s":    this.neighbors[0][0],
+            "d":    this.neighbors[2][0],
+            "a":    this.neighbors[3][0],
+            "path": "path"
+        }
+        let info = [0,1]
+        if (elem.value === "P"){ 
+            console.log(elem.value)
+            let dir = this.userInput
+            info[0] = dir
+            console.log(dir)
+            console.log(this.location)
+            }
+        else if(elem.value == "W"){
+            let queue = [[`${elem.location[0]},${elem.location[1]}`]]
+            let visited = []
+            while (queue.length > 0){
+                let path = queue.shift()
+                info[1] = path
+                let location = path[path.length -1]
+                if (location == `${enem.location[0]},${enem.location[1]}`){
+                    console.log("found")
+                    break
+                }
+                else if(!(visited.includes(location))){
+                    for (const neighbor of this.connections[location]){
+                       let new_path = list(path)
+                        new_path.push(`${neighbor[0]},${neighbor[1]}`)
+                        queue.push(new_path)
+                    }
+                    visited.push(location)
+                }
+
+                }  
+            
+            console.log(info[1])
+            let  choices = ["w","s","d","a","path","path","path","path","path","path","path"]
+            const random = Math.floor(Math.random() * choices.length);
+            info[0] = choices[random]
+            
+        }
+       
+        if (directions[info[0]] != "X"){
+            console.log(info[0])
+            if (info[0] =="path"){
+                info[1].shift(0)
+                let to_find = info[1][0]
+                console.log(to_find)
+                for (let i = 0; i < this.neighbors.length; i ++){
+                   
+                    if (this.neighbors[i][1] == to_find){
+                        if (i == 0){
+                            info[0] = "s"
+                        }
+                        if (i == 1){
+                            info[0] = "w"
+                        }
+                        if (i == 2){
+                            info[0] = "d"
+                        }
+                        if (i == 3){
+                            info[0] = "a"
+                        }
+                    }}console.log(info[0])}
+
+            if (info[0] == "w"){
+                console.log(info[0])
+                this.location[0] -= 1 
+                this.map[this.location[0] + 1][this.location[1]] = "."
+            }
+            if (info[0] == "s"){
+                this.location[0] += 1
+                this.map [this.location[0] - 1][this.location[1]] = "."
+            }
+            if (info[0] == "d"){
+                this.location[1] += 1
+                this.map[this.location[0]] [this.location[1] - 1] = "."
+            }
+            if (info[0]== "a"){
+                this.location[1] -= 1
+                this.map[this.location[0]][this.location[1] + 1] = "."
+            }
+           }
+
+    
+
+            if (elem.value == "P"){
+                console.log(this.location)
+                if(this.map [this.location[0]][this.location[1]] == "$"){
+                    this.points += 1
+                }
+                }
+            this.map[this.location[0]][this.location[1]] = elem.value
+            this.get_neighbors()
+           
+            
+    }
+        }
+
+   
