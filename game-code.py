7a113329c18ab781b023b5355d7ff7ff6c70e097
John Rojcewicz <rojcewiczj@gmail.com>
2020-09-06 16:52:25 -0500

    developed a menu system

--- game-code.py
+++ game-code.py
@@ -1,6 +1,10 @@
 import random
+import curses
 
-# Play in your terminal with w, s, d, a as your directional commands. Try to get all the money without getting caught by the "W's"!
+
+
+
+
 
 class Ef:
     def __init__(self, value, mappy, map_object):
@@ -135,61 +139,88 @@ class matrix:
         self.arrays[x][y] = value.value
         value.location = [x, y]
     
-            
-
-running = True
-map = matrix()
-map.generate()
-map.adaj_dict_gen()
-elem = Ef("F", map.arrays, map)
-enem = Ef("W", map.arrays, map)
-enem1 = Ef("W", map.arrays, map)
-enem2 = Ef("W", map.arrays, map)
-enem3 = Ef("W", map.arrays, map)
-money = Ef("$", map.arrays, map)
-ex = Ef("X", map.arrays, map)
-map.insert(1,2, elem)
-map.insert(3,3, ex)
-map.insert(7,4, ex)
-map.insert(2,7, ex)
-map.insert(3,9, ex)
-map.insert(9,13, ex)
-map.insert(13,18, ex)
-map.insert(11,16, ex)
-map.insert(15,5, ex)
-map.insert(17,2, ex)
-map.insert(14,6, ex)
-map.insert(17,9, ex)
-map.adaj_dict_gen()
-map.insert(4,5, enem)
-map.insert(2,3, enem1)
-map.insert(5,6, enem2)
-map.insert(15,16, enem3)
-map.insert(4,5, money)
-map.insert(2,6, money)
-map.insert(5,8, money)
-map.insert(8,1, money)
-map.insert(15,5, money)
-map.insert(12,11, money)
-map.insert(11,18, money)
-map.insert(16,3, money)
-map.insert(6,13, money)
-map.insert(9,9, money)
-map.insert(17,12, money)
-
-elem.get_neighbors()
-map.print_map()
+                
 
 
-while running:
-    elem.move(elem, enem)
-    enem.move(enem, elem)
-    enem1.move(enem1, elem)
-    enem2.move(enem2, elem)
-    enem3.move(enem3, elem)
-    for row in map.arrays:
+scores = []
+running = False
+main_options = ["Play", "Scoreboard"]
+back_options = ["Back To Menu!", "Play Again!"]
+
+
+def main_menu():
+    
+    map = matrix()
+    map.generate()
+    map.adaj_dict_gen()
+    elem = Ef("F", map.arrays, map)
+    enem = Ef("W", map.arrays, map)
+    enem1 = Ef("W", map.arrays, map)
+    enem2 = Ef("W", map.arrays, map)
+    enem3 = Ef("W", map.arrays, map)
+    money = Ef("$", map.arrays, map)
+    ex = Ef("X", map.arrays, map)
+    map.insert(1,2, elem)
+    map.insert(3,3, ex)
+    map.insert(7,4, ex)
+    map.insert(2,7, ex)
+    map.insert(3,9, ex)
+    map.insert(9,13, ex)
+    map.insert(13,18, ex)
+    map.insert(11,16, ex)
+    map.insert(15,5, ex)
+    map.insert(17,2, ex)
+    map.insert(14,6, ex)
+    map.insert(17,9, ex)
+    map.adaj_dict_gen()
+    map.insert(4,5, enem)
+    map.insert(2,3, enem1)
+    map.insert(5,6, enem2)
+    map.insert(15,16, enem3)
+    map.insert(4,5, money)
+    map.insert(2,6, money)
+    map.insert(5,8, money)
+    map.insert(8,1, money)
+    map.insert(15,5, money)
+    map.insert(12,11, money)
+    map.insert(11,18, money)
+    map.insert(16,3, money)
+    map.insert(6,13, money)
+    map.insert(9,9, money)
+    map.insert(17,12, money)
+    elem.get_neighbors()
+    for options in main_options:
+        print(options)
+    main_choice =input("To play the game type 'p' and press enter.....To check the Scoreboard type 's' and press enter:")
+    if main_choice == 'p':
+        running = True
+        print(map.print_map())
+        while running:
+            elem.move(elem, enem)
+            enem.move(enem, elem)
+            enem1.move(enem1, elem)
+            enem2.move(enem2, elem)
+            enem3.move(enem3, elem    )
+            for row in map.arrays:
                 print(row)
-    print("TOTAL POINTS: ", map.points)
-    if map.arrays[elem.location[0]][elem.location[1]] == "W":
-        running = False
-print("YOU HAVE BEEN CONSUMED!")  
+            print("TOTAL POINTS: ", map.points)
+            if map.arrays[elem.location[0]][elem.location[1]] == "W":
+                scores.append(map.points)
+                map.points = 0
+                if len(scores) > 10:
+                    scores.pop(0)
+                running = False
+                print("YOU HAVE BEEN CONSUMED!")  
+                main_menu()
+    elif main_choice == 's':
+        for score in scores:
+            print(score)
+        main_menu()
+
+
+main_menu()
+
+
+
+                
+           
